af1f8a2361a84337a98e0c097a5ad7fee46f0f81
Zhiwen Xu <zxu@terrapower.com>
2017-03-08 04:53:10 -0800

    Develop Powershell script to copy inputs and reference outputs

---
+++ vnv/base/copy_tests.ps1
@@ -0,0 +1,29 @@
+# clean files
+del inp\*, out\*
+
+# copy ENDF data files
+copy  ..\..\test\resources\*  inp
+
+# loop through case directories under test
+foreach($caseDir in $(Get-ChildItem ..\..\test\?? -Directory))
+{
+    $input = Join-Path $caseDir.FullName 'input'
+    $caseInp = Join-Path 'inp' ('case' + $caseDir.BaseName + '.inp')
+    if(Test-Path $input)
+    {
+        Write-Host 'copying', $input, 'to', $caseInp, '...' -foregroundcolor red -backgroundcolor yellow
+        copy $input $caseInp
+    }
+    else
+    {
+        throw '$input not found.'
+    }
+
+    foreach($referenceTape in $(Get-ChildItem $caseDir -Filter referenceTape??))
+    {
+        $tapeNum = $referenceTape.BaseName.SubString($referenceTape.BaseName.Length - 2)
+        $caseTape = Join-Path 'out' ('case' + $caseDir.BaseName + '.tape' + $tapeNum)
+        Write-Host 'copying', $referenceTape.FullName, 'to', $caseTape
+        copy $referenceTape.FullName $caseTape
+    }
+}
