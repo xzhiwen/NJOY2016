cb7fefbcbe1370083c19dfe55591aade10d3f254
Zhiwen Xu <zxu@terrapower.com>
2017-03-07 16:32:44 -0800

    Compile njoy2016_00.exe

--- src/nmake/njoy2016.bat
+++ src/nmake/njoy2016.bat
@@ -1,2 +1,2 @@
-nmake TARGET="%~n0.exe" 2>&1 | tee.exe %~n0.log
-fciv.exe -both %~n0.exe 2>&1 | tee.exe -a %~n0.log
+nmake  TARGET="njoy2016_00.exe" 2>&1 | tee.exe %~n0.log
+fciv.exe -both njoy2016_00.exe  2>&1 | tee.exe -a %~n0.log
