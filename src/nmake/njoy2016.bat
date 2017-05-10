c862ecd71fa11046ad5b7e7a3d4ee1dafe093a22
Zhiwen Xu <xzhiwen@gmail.com>
2017-05-10 10:35:51 -0700

    Compile NJOY2016.15 code

--- src/nmake/njoy2016.bat
+++ src/nmake/njoy2016.bat
@@ -1,2 +1,2 @@
-nmake  TARGET="njoy2016_01.exe" 2>&1 | tee.exe %~n0.log
-fciv.exe -both njoy2016_01.exe  2>&1 | tee.exe -a %~n0.log
+nmake  TARGET="njoy2016_15.exe" 2>&1 | tee.exe %~n0.log
+fciv.exe -both njoy2016_15.exe  2>&1 | tee.exe -a %~n0.log
